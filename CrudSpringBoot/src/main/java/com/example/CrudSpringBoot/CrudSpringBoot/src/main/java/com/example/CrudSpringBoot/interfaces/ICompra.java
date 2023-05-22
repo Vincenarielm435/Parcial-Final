9eebf676ab97b74a6484ef3ed5bbf631a2f2d1ab
Vincenarielm435 <131629321+Vincenarielm435@users.noreply.github.com>
2023-05-22 10:54:43 -0500

    Delete CrudSpringBoot/src/main/java/com/example/CrudSpringBoot/CrudSpringBoot directory

--- CrudSpringBoot/src/main/java/com/example/CrudSpringBoot/CrudSpringBoot/src/main/java/com/example/CrudSpringBoot/interfaces/ICompra.java
+++
@@ -1,8 +0,0 @@
-package com.example.CrudSpringBoot.interfaces;
-
-import com.example.CrudSpringBoot.modelo.Compra;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ICompra extends CrudRepository<Compra, Integer> {
-}
-
